aea7b84bb5ea86bc3be67bdd2e3d77d31a5938fb
Farrukhshahzad1106 <shahzadfarrukh621@gmail.com>
2026-07-06 23:26:39 +0530

    updating the repo

--- Nodejs/xyz.js
+++ Nodejs/xyz.js
@@ -1,1 +1,3 @@
 console.log("logging from xyz.js");
+
+console.log("As per module caching, this file will be executed only once even if we import it multiple times in different files. This is because nodejs caches the module after the first time it is imported and then returns the cached version of the module for subsequent imports.");
