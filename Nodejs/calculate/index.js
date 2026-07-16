c9e55d7a9f9966deea93d94565be3bacb0f4860c
Farrukhshahzad1106 <shahzadfarrukh621@gmail.com>
2026-07-16 22:13:31 +0530

    created the multiply any function

--- Nodejs/calculate/index.js
+++ Nodejs/calculate/index.js
@@ -1,8 +1,8 @@
 const {calculateSum} = require('./sum.js');
-const {calculateMultiply} = require('./multiply.js');
+const {calculateMultiply, multiplyAny} = require('./multiply.js');
 const {substract} = require('./substract.js');
 const {calculateQuotient, calculateRemainder} = require('./divide.js');
 
 
-module.exports = { calculateSum, calculateMultiply, substract, calculateQuotient, calculateRemainder}; 
+module.exports = { calculateSum, calculateMultiply, multiplyAny, substract, calculateQuotient, calculateRemainder}; 
 // exporting all the modules from this file so that we can import them in app.js file
--- Nodejs/calculate/multiply.js
+++ Nodejs/calculate/multiply.js
@@ -2,6 +2,22 @@ function calculateMultiply(a, b) {
   return a * b;
 }
 
+function multiplyAny(...args) {
+  if (args.length === 0) {
+    throw new Error("At least one number is required for multiplication.");
+  }
+  let result = args[0];
+  for (let i=1; i<args.length; i++) {
+    const num = args[i];
+    if (typeof num !== 'number') {
+      throw new Error("All arguments must be numbers.");
+    }
+    result *= num;
+  }
+  return result;
+}
+
 module.exports = {
-  calculateMultiply
+  calculateMultiply,
+  multiplyAny
 };
