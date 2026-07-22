b97f3fed18bf21846c6a518680a71047b439a4f6
Farrukhshahzad1106 <shahzadfarrukh621@gmail.com>
2026-07-22 21:35:57 +0530

    updating the repo

--- Nodejs/app.js
+++ Nodejs/app.js
@@ -19,7 +19,7 @@ const fs = require("node:fs"); // importing nodejs built-in module called fs
 
 var name = "Namaste Nodejs, learning Backend development using Nodejs";
 
-var a = 1;
+var a = 21;
 var b = 17;
 const c = 18;
 var d = 0;
