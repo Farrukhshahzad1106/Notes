a7f90c76f3b6a71c7349be827230df910fd2a30c
Farrukhshahzad1106 <shahzadfarrukh621@gmail.com>
2026-07-22 21:38:12 +0530

    created sumAny function

--- Nodejs/calculate/sum.js
+++ Nodejs/calculate/sum.js
@@ -10,9 +10,24 @@ function calculateSum(a, b) {
   return a + b;
 }
 
+function sumAny(...args) {
+  if (args.length === 0) {
+    throw new Error("At least one number is required for summation.");
+  }
+  let result = 0;
+  for (const num of args) {
+    if (typeof num !== 'number') {
+      throw new Error("All arguments must be numbers.");
+    }
+    result += num;
+  }
+  return result;
+}
+
 var x = 50;
 
 module.exports = {
     x,
-  calculateSum
+  calculateSum,
+  sumAny
 };
