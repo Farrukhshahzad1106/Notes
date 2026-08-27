3810e01138447a287748cebcee1c5b7ba938fda3
Farrukhshahzad1106 <shahzadfarrukh621@gmail.com>
2026-08-27 23:43:07 +0530

    updating the repo

--- Nodejs/calculate/multiply.js
+++ Nodejs/calculate/multiply.js
@@ -15,7 +15,7 @@ function multiplyAny(...args) {
     if (typeof num !== 'number') {
       throw new Error("All arguments must be numbers.");
     }
-    result *= num;
+    result = result * num;
   }
   return result;
 }
